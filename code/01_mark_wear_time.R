e536d4c8550ade3d8ea76a0ed20cddadf20d0852
Lucas Veras <lucasdsveras@gmail.com>
2020-04-13 18:21:05 +0100

    Change path to data

--- code/01_mark_wear_time.R
+++ code/01_mark_wear_time.R
@@ -6,12 +6,16 @@ library(read.gt3x)
 
 # Read data ---------------------------------------------------------------
 
+data_dir <- "/Volumes/LVERAS/data/"
+agd_path <- paste(data_dir, "068_4th_60sec_epoch.agd", sep = "")
+raw_path <- paste(data_dir, "068_4th.gt3x", sep = "")
+
 # agd file (epoch)
-agd <- readActigraph("data/068_4th_60sec_epoch.agd")
+agd <- readActigraph(agd_path)
 
 # gt3x file (raw)
 raw <- read.gt3x(
-  "data/068_4th.gt3x", imputeZeroes = TRUE, asDataFrame = TRUE
+  raw_path, imputeZeroes = TRUE, asDataFrame = TRUE
 )
 
 # Process -----------------------------------------------------------------
@@ -42,10 +46,18 @@ summary %>%
 # Export ------------------------------------------------------------------
 
 # Wear time log 
-if (dir.exists("data/wear_time") == FALSE) {
-  dir.create("data/wear_time")
+wt_dir <- paste(data_dir, "wear_time/", sep = "")
+if (dir.exists(wt_dir) == FALSE) {
+  dir.create(wt_dir)
 }
-write_csv(summary, "data/wear_time/068.csv")
+write_csv(summary, paste(wt_dir, "068.csv", sep = ""))
 
-# Raw data to csv
-write_csv(raw, "data/068_4th_gt3x.csv")
+# Raw data
+# Select only the acc axes
+raw <- raw[, 1:3]
+# Write into a txt file
+write_delim(
+  raw, 
+  paste(data_dir, "068_4th_raw.txt", sep = ""), 
+  delim = ","
+)
