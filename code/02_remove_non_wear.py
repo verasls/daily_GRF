2d7c9b59b3f5687ba6273775b1d3c5f73dba8125
Lucas Veras <lucasdsveras@gmail.com>
2020-04-13 18:21:36 +0100

    Change path to data

--- code/02_remove_non_wear.py
+++ code/02_remove_non_wear.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import time
 
 
-raw_acc = pd.read_csv("../data/068_4th_gt3x.csv", delimiter=",")
-print(raw_acc.iloc[3864100 - 1, :])
+# Get path to data
+data_dir = "/Volumes/LVERAS/data/"
+log_path = data_dir + "wear_time/068.csv"
+raw_path = data_dir + "068_4th_raw.txt"
+
+# Read wear time log
+log = pd.read_csv(log_path)
+
+# Read raw data file
+start_time = time.time()
+data = pd.read_csv(raw_path)
+print("pd.read_csv took %s seconds" % (time.time() - start_time))
