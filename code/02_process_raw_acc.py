e650b2d8024e9df4f990389c1a5d96663fe9af09
Lucas Veras <lucasdsveras@gmail.com>
2020-04-17 19:21:32 +0100

    Re-organize file to analyse all data

--- code/02_process_raw_acc.py
+++ code/02_process_raw_acc.py
@@ -1,87 +1,108 @@
+import glob
+import os
+import pickle
 import pandas as pd
 import numpy as np
 from scipy import signal
-import os
-import pickle
 
 
-# Get path to data
-data_dir = "/Volumes/LVERAS/data/"
-log_path = data_dir + "wear_time/068.csv"
-raw_path = data_dir + "068_4th_raw.txt"
+# Getting paths
+# General
+data_dir = "/Volumes/LVERAS/daily_GRF/data/"
+output_dir = "/Volumes/LVERAS/daily_GRF/output/"
 
-# Read wear time log
-log = pd.read_csv(log_path)
+# Specific
+log_data = output_dir + "02_wear_time_logs/"
+raw_data = output_dir + "01_raw_acc_data/"
 
-# Get info from log
-info = {"duration": [], "week_day": [], "start": [], "end": []}
-for i in range(0, len(log.index)):
-    info["duration"].append(log.iloc[i, 6])
-    info["week_day"].append(str(log.iloc[i, 3])[:3])
-    info["start"].append(log.iloc[i, 7] - 1)
-    info["end"].append(log.iloc[i, 8] - 1)
-print("Writing wear time info into a file")
-info_dir = data_dir + "info/"
-if os.path.exists(info_dir) is False:
-    os.mkdir(info_dir)
-info_path = info_dir + "068_info.txt"
-with open(info_path, "wb") as handle:
-    pickle.dump(info, handle)
+log_output = output_dir + "03_wear_time_info/"
+raw_output = output_dir + "04_acc_peaks/"
 
-# Read raw data file
-print("Reading raw data")
-data = pd.read_csv(raw_path)
-# Put each axis into a ndarray
-aX = data.iloc[:, 0].to_numpy()
-aY = data.iloc[:, 1].to_numpy()
-aZ = data.iloc[:, 2].to_numpy()
+# Create output directory if it does not exist
+if os.path.exists(log_output) is False:
+    os.mkdir(log_output)
+if os.path.exists(raw_output) is False:
+    os.mkdir(raw_output)
 
-# Filter acceleration signal
-# Create the lowpass filter
-samp_freq = 100
-N = 4  # Filter order
-cutoff = 20  # cut-off frequency (Hz)
-fnyq = samp_freq / 2  # Nyquist frequency (half of the sampling frequency)
-Wn = cutoff / fnyq  # Filter parameter
+# List files
+log_files = sorted(glob.glob(log_data + "*.txt"))
+raw_files = sorted(glob.glob(raw_data + "*.txt"))
 
-b, a = signal.butter(N, Wn, btype="low")
+# Process raw data
+for i in range(0, len(log_files)):
+    print("Processing file", i + 1, "out of", len(log_files))
 
-# Process signal
-print("Filtering acceleration signal")
-aX = signal.filtfilt(b, a, aX)
-aY = signal.filtfilt(b, a, aY)
-aZ = signal.filtfilt(b, a, aZ)
+    # Read wear time log
+    print("Reading wear time log file:", log_files[i][-25:])
+    log = pd.read_csv(log_files[i])
 
-# Compute resultant vector
-aR = np.sqrt(aX ** 2 + aY ** 2 + aZ ** 2)
+    # Get info from log
+    info = {"duration": [], "week_day": [], "start": [], "end": []}
+    for j in range(0, len(log.index)):
+        info["duration"].append(log.iloc[j, 6])
+        info["week_day"].append(str(log.iloc[j, 3])[:3])
+        info["start"].append(log.iloc[j, 7] - 1)
+        info["end"].append(log.iloc[j, 8] - 1)
 
-# Group wear time blocks in a dictionary
-print("Grouping the acceleration signal into wear time blocks")
-blocks = {}
-for i in range(0, len(info["start"])):
-    key_name = "block_" + str(i + 1)
-    blocks[key_name] = aR[info["start"][i]:info["end"][i]]
+    print("Writing wear time info")
+    # Writing info dict in to a file
+    log_output_file = log_output + log_files[i][-25:-8] + "_info.txt"
+    with open(log_output_file, "wb") as handle:
+        pickle.dump(info, handle)
+    print("File written:", log_files[i][-25:-8] + "_info.txt")
 
-# Find peaks for all blocks
-# Peaks criteria
-height = 1.3
-distance = 0.4 * samp_freq  # seconds * sampling frequency
-# Find peaks
-for i in range(0, len(blocks)):
-    print("Finding peaks for block", str(i + 1))
-    acc_signal = blocks[list(blocks)[i]]
-    peaks, properties = signal.find_peaks(acc_signal, height=height,
-                                          distance=distance)
-    # Substitute the acceleration signal by the peaks magnitude in the dict
-    blocks[list(blocks)[i]] = properties["peak_heights"]
+    # Read raw data file
+    print("Reading raw accelerometer data file:", raw_files[i][-15:])
+    data = pd.read_csv(raw_files[i])
+    # Put each axis into a ndarray
+    aX = data.iloc[:, 0].to_numpy()
+    aY = data.iloc[:, 1].to_numpy()
+    aZ = data.iloc[:, 2].to_numpy()
 
-# Write blocks dictionary into a file
-print("Writing the acceleration peaks magnitude into a file")
-peaks_dir = data_dir + "peaks/"
-if os.path.exists(peaks_dir) is False:
-    os.mkdir(peaks_dir)
-peaks_path = peaks_dir + "068_peaks.txt"
-with open(peaks_path, "wb") as handle:
-    pickle.dump(blocks, handle)
+    # Filter acceleration signal
+    # Create the lowpass filter
+    samp_freq = 100
+    N = 4  # Filter order
+    cutoff = 20  # cut-off frequency (Hz)
+    fnyq = samp_freq / 2  # Nyquist frequency (half of the sampling frequency)
+    Wn = cutoff / fnyq  # Filter parameter
+
+    b, a = signal.butter(N, Wn, btype="low")
+
+    # Process signal
+    print("Filtering acceleration signal")
+    aX = signal.filtfilt(b, a, aX)
+    aY = signal.filtfilt(b, a, aY)
+    aZ = signal.filtfilt(b, a, aZ)
+
+    # Compute resultant vector
+    aR = np.sqrt(aX ** 2 + aY ** 2 + aZ ** 2)
+
+    # Group wear time blocks in a dictionary
+    print("Grouping the acceleration signal into wear time blocks")
+    blocks = {}
+    for j in range(0, len(info["start"])):
+        key_name = "block_" + str(j + 1)
+        blocks[key_name] = aR[info["start"][j]:info["end"][j]]
+
+    # Find peaks for all blocks
+    # Peaks criteria
+    height = 1.3
+    distance = 0.4 * samp_freq  # seconds * sampling frequency
+    # Find peaks
+    for j in range(0, len(blocks)):
+        print("Finding peaks for block", str(j + 1))
+        acc_signal = blocks[list(blocks)[j]]
+        peaks, properties = signal.find_peaks(acc_signal, height=height,
+                                              distance=distance)
+        # Substitute the acceleration signal by the peaks magnitude in the dict
+        blocks[list(blocks)[j]] = properties["peak_heights"]
+
+    # Write blocks dictionary into a file
+    print("Writing the acceleration peaks magnitude into a file")
+    raw_output_file = raw_output + raw_files[i][-15:-8] + "_acc_peaks.txt"
+    with open(raw_output_file, "wb") as handle:
+        pickle.dump(blocks, handle)
+    print("File written:", raw_files[i][-15:-8] + "_acc_peaks.txt")
 
 print("Done!")
