fe221d0b8094e95af064529746a9ea330148d8ce
Lucas Veras <lucasdsveras@gmail.com>
2020-04-17 18:31:10 +0100

    Re-organize script to analyse all files in a directory

--- code/02_mark_wear_time.R
+++ code/02_mark_wear_time.R
@@ -1,64 +1,71 @@
 # Load packages -----------------------------------------------------------
 
-library(tidyverse)
-library(PhysicalActivity)
-library(read.gt3x)
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(PhysicalActivity))
 
 # Read data ---------------------------------------------------------------
 
-data_dir <- "/Volumes/LVERAS/data/"
-agd_path <- paste(data_dir, "068_4th_60sec_epoch.agd", sep = "")
-raw_path <- paste(data_dir, "068_4th.gt3x", sep = "")
+# Getting paths -----------------------------------------------------------
 
-# agd file (epoch)
-agd <- readActigraph(agd_path)
+# General
+data_dir <- "/Volumes/LVERAS/daily_GRF/data/"
+output_dir <- "/Volumes/LVERAS/daily_GRF/output/"
 
-# gt3x file (raw)
-raw <- read.gt3x(
-  raw_path, imputeZeroes = TRUE, asDataFrame = TRUE
-)
+# Specific
+agd_data <- str_c(data_dir, "agd/", sep = "")
+agd_output <- str_c(output_dir, "02_wear_time_logs/", sep = "")
 
-# Process -----------------------------------------------------------------
-
-# Mark wear and non-wear time using agd epoch file
-marked <- wearingMarking(
-  dataset = agd,
-  frame = 90,
-  perMinuteCts = 1,
-  getMinuteMarking = TRUE
-)
-
-# Summarise
-summary <- sumVct(datavct = marked) %>% as_tibble()
-summary$weekday <- str_to_upper(summary$weekday)
-summary$weekday <- as_factor(summary$weekday)
-# Get start and end lines for raw data (to be used in python script)
-summary <- summary %>% 
-  mutate(
-    start_idx = (start * 60 * 100) - (59 * 100),
-    end_idx = (end * 60 * 100) - (59 * 100)
-  )
-
-# Get wear time per day (min)
-summary %>% 
-  group_by(weekday) %>% 
-  summarise(wear_time = sum(duration))
-
-# Export ------------------------------------------------------------------
-
-# Wear time log 
-wt_dir <- paste(data_dir, "wear_time/", sep = "")
-if (dir.exists(wt_dir) == FALSE) {
-  dir.create(wt_dir)
+# Create output directory if it does not exist
+if (dir.exists(agd_output) == FALSE) {
+  dir.create(agd_output, recursive = TRUE)
 }
-write_csv(summary, paste(wt_dir, "068.csv", sep = ""))
 
-# Raw data
-# Select only the acc axes
-raw <- raw[, 1:3]
-# Write into a txt file
-write_delim(
-  raw, 
-  paste(data_dir, "068_4th_raw.txt", sep = ""), 
-  delim = ","
-)
+# List files in data directory
+files <- list.files(agd_data)
+
+# Marking wear time -------------------------------------------------------
+
+for (i in 1:length(files)) {
+  # Read agd file
+  message <- str_c(
+    "Reading file ", i, " out of ", length(files), ": ", files[i], sep = ""
+  )
+  print(message)
+  
+  data_file <- str_c(agd_data, files[i], sep = "")
+  agd <- readActigraph(data_file)
+  
+  # Mark wear and non-wear time using agd epoch file
+  print("Marking wear time")
+  marked <- wearingMarking(
+    dataset = agd,
+    frame = 90,
+    perMinuteCts = 1,
+    getMinuteMarking = TRUE
+  )
+  
+  # Summarise wear time information
+  wear_time_log <- sumVct(datavct = marked) %>% as_tibble()
+  wear_time_log$weekday <- str_to_upper(wear_time_log$weekday)
+  wear_time_log$weekday <- as_factor(wear_time_log$weekday)
+  
+  # Get start and end lines for raw data (to be used in python script)
+  wear_time_log <- wear_time_log %>% 
+    mutate(
+      start_idx = (start * 60 * 100) - (59 * 100),
+      end_idx = (end * 60 * 100) - (59 * 100)
+    )
+  
+  # Write log into txt
+  print("Writing wear time log")
+  output_file <- str_c(str_sub(files[i], 1, 7), "_wear_time_log.txt", sep = "")
+  write_delim(
+    wear_time_log,
+    str_c(agd_output, output_file, sep = ""),
+    delim = ","
+  )
+  message <- str_c("File written: ", output_file, sep = "")
+  print(message)
+}
+
+print("Done!")
