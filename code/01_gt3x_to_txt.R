55265841ea83dff87db06e7a034aad3663d380fc
Lucas Veras <lucasdsveras@gmail.com>
2020-04-18 11:09:40 +0100

    Extract ID and eval numbers from the filenames

--- code/01_gt3x_to_txt.R
+++ code/01_gt3x_to_txt.R
@@ -10,27 +10,31 @@ data_dir <- "/Volumes/LVERAS/daily_GRF/data/"
 output_dir <- "/Volumes/LVERAS/daily_GRF/output/"
 
 # Specific
-gt3x_data <- str_c(data_dir, "gt3x/", sep = "")
-gt3x_output <- str_c(output_dir, "01_raw_acc_data/", sep = "")
+gt3x_data_dir <- str_c(data_dir, "gt3x/", sep = "")
+gt3x_output_dir <- str_c(output_dir, "01_raw_acc_data/", sep = "")
 
 # Create output directory if it does not exist
-if (dir.exists(gt3x_output) == FALSE) {
-  dir.create(gt3x_output, recursive = TRUE)
+if (dir.exists(gt3x_output_dir) == FALSE) {
+  dir.create(gt3x_output_dir, recursive = TRUE)
 }
 
 # List files in data directory
-files <- list.files(gt3x_data)
+files <- list.files(gt3x_data_dir)
 
 # Converting gt3x into txt ------------------------------------------------
 
 for (i in 1:length(files)) {
+  # Get info from file name
+  ID_num <- str_sub(files[i], 1, 3)
+  eval_num <- str_sub(files[i], 5, 7)
+  
   # Read gt3x file (raw)
   message <- str_c(
     "Reading file ", i, " out of ", length(files), ": ", files[i], sep = ""
   )
   print(message)
   
-  data_file <- str_c(gt3x_data, files[i], sep = "")
+  data_file <- str_c(gt3x_data_dir, files[i], sep = "")
   gt3x <- read.gt3x(
     data_file, imputeZeroes = TRUE, asDataFrame = TRUE
   )
@@ -39,10 +43,10 @@ for (i in 1:length(files)) {
   
   print("Converting gt3x into txt")
   # Write raw data into a txt file
-  output_file <- str_c(str_sub(files[i], 1, -6), "_raw.txt", sep = "")
+  output_file <- str_c(ID_num, eval_num, "raw.txt", sep = "_")
   write_delim(
     gt3x, 
-    str_c(gt3x_output, output_file, sep = ""), 
+    str_c(gt3x_output_dir, output_file, sep = ""), 
     delim = ","
   )
   message <- str_c("File written: ", output_file, sep = "")
