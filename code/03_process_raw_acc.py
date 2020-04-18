2e57c2ff5bae9cf2c478bb7ad41aefc4e734e112
Lucas Veras <lucasdsveras@gmail.com>
2020-04-18 12:05:51 +0100

    Process raw acceleration only if the output file does not already exists

--- code/03_process_raw_acc.py
+++ code/03_process_raw_acc.py
@@ -70,59 +70,66 @@ for i in range(0, len(log_files)):
         message = message + " already exists"
         print(message)
 
-    # Read raw data file
-    print("Reading raw accelerometer data file:", raw_files[i])
-    data = pd.read_csv(raw_data_dir + raw_files[i])
-    # Put each axis into a ndarray
-    aX = data.iloc[:, 0].to_numpy()
-    aY = data.iloc[:, 1].to_numpy()
-    aZ = data.iloc[:, 2].to_numpy()
-
-    # Filter acceleration signal
-    # Create the lowpass filter
-    samp_freq = 100
-    N = 4  # Filter order
-    cutoff = 20  # cut-off frequency (Hz)
-    fnyq = samp_freq / 2  # Nyquist frequency (half of the sampling frequency)
-    Wn = cutoff / fnyq  # Filter parameter
-
-    b, a = signal.butter(N, Wn, btype="low")
-
-    # Process signal
-    print("Filtering acceleration signal")
-    aX = signal.filtfilt(b, a, aX)
-    aY = signal.filtfilt(b, a, aY)
-    aZ = signal.filtfilt(b, a, aZ)
-
-    # Compute resultant vector
-    aR = np.sqrt(aX ** 2 + aY ** 2 + aZ ** 2)
-
-    # Group wear time blocks in a dictionary
-    print("Grouping the acceleration signal into wear time blocks")
-    blocks = {}
-    for j in range(0, len(info["start"])):
-        key_name = "block_" + str(j + 1)
-        blocks[key_name] = aR[info["start"][j]:info["end"][j]]
-
-    # Find peaks for all blocks
-    # Peaks criteria
-    height = 1.3
-    distance = 0.4 * samp_freq  # seconds * sampling frequency
-    # Find peaks
-    for j in range(0, len(blocks)):
-        print("Finding peaks for block", str(j + 1))
-        acc_signal = blocks[list(blocks)[j]]
-        peaks, properties = signal.find_peaks(acc_signal, height=height,
-                                              distance=distance)
-        # Substitute the acceleration signal by the peaks magnitude in the dict
-        blocks[list(blocks)[j]] = properties["peak_heights"]
-
-    # Write blocks dictionary into a file
-    print("Writing the acceleration peaks magnitude into a file")
+    # Check if output file already exists for the current ID and process the
+    # raw acceleration only if not
     raw_output_file = raw_output_dir + ID_num + "_" + eval_num
     raw_output_file = raw_output_file + "_acc_peaks.txt"
-    with open(raw_output_file, "wb") as handle:
-        pickle.dump(blocks, handle)
-    print("File written:", ID_num + "_" + eval_num + "_acc_peaks.txt")
+    if os.path.exists(raw_output_file) is False:
+        # Read raw data file
+        print("Reading raw accelerometer data file:", raw_files[i])
+        data = pd.read_csv(raw_data_dir + raw_files[i])
+        # Put each axis into a ndarray
+        aX = data.iloc[:, 0].to_numpy()
+        aY = data.iloc[:, 1].to_numpy()
+        aZ = data.iloc[:, 2].to_numpy()
+
+        # Filter acceleration signal
+        # Create the lowpass filter
+        samp_freq = 100
+        N = 4  # Filter order
+        cutoff = 20  # cut-off frequency (Hz)
+        fnyq = samp_freq / 2  # Nyquist frequency
+        Wn = cutoff / fnyq  # Filter parameter
+
+        b, a = signal.butter(N, Wn, btype="low")
+
+        # Process signal
+        print("Filtering acceleration signal")
+        aX = signal.filtfilt(b, a, aX)
+        aY = signal.filtfilt(b, a, aY)
+        aZ = signal.filtfilt(b, a, aZ)
+
+        # Compute resultant vector
+        aR = np.sqrt(aX ** 2 + aY ** 2 + aZ ** 2)
+
+        # Group wear time blocks in a dictionary
+        print("Grouping the acceleration signal into wear time blocks")
+        blocks = {}
+        for j in range(0, len(info["start"])):
+            key_name = "block_" + str(j + 1)
+            blocks[key_name] = aR[info["start"][j]:info["end"][j]]
+
+        # Find peaks for all blocks
+        # Peaks criteria
+        height = 1.3
+        distance = 0.4 * samp_freq  # seconds * sampling frequency
+        # Find peaks
+        for j in range(0, len(blocks)):
+            print("Finding peaks for block", str(j + 1))
+            acc_signal = blocks[list(blocks)[j]]
+            peaks, properties = signal.find_peaks(acc_signal, height=height,
+                                                  distance=distance)
+            # Substitute acceleration signal by peaks magnitude in the dict
+            blocks[list(blocks)[j]] = properties["peak_heights"]
+
+        # Write blocks dictionary into a file
+        print("Writing the acceleration peaks magnitude into a file")
+        with open(raw_output_file, "wb") as handle:
+            pickle.dump(blocks, handle)
+        print("File written:", ID_num + "_" + eval_num + "_acc_peaks.txt")
+    else:
+        message = "File " + ID_num + "_" + eval_num + "_acc_peaks.txt"
+        message = message + " already exists"
+        print(message)
 
 print("Done!")
