f382b6f572b76d38686a0ad2682a80b4a6a89ccd
Lucas Veras <lucasdsveras@gmail.com>
2020-04-20 19:05:12 +0100

    Turn into a function to be run from the command line

--- code/01_gt3x_to_txt.R
+++ code/01_gt3x_to_txt.R
@@ -1,70 +1,69 @@
-# Load packages -----------------------------------------------------------
+main <- function() {
+  suppressPackageStartupMessages(require(tidyverse, quietly = TRUE))
+  suppressPackageStartupMessages(require(read.gt3x, quietly = TRUE))
 
-suppressPackageStartupMessages(library(tidyverse, quietly = TRUE))
-suppressPackageStartupMessages(library(read.gt3x, quietly = TRUE))
+  args <- commandArgs(trailingOnly = TRUE)
+  data_dir <- args[1]
+  output_dir <- args[2]
+    
+  # Set paths
+  gt3x_data_dir <- str_c(data_dir, "gt3x/", sep = "")
+  gt3x_output_dir <- str_c(output_dir, "01_raw_acc_data/", sep = "")
 
-# Getting paths -----------------------------------------------------------
-
-# General
-data_dir <- "/Volumes/LVERAS/daily_GRF/data/"
-output_dir <- "/Volumes/LVERAS/daily_GRF/output/"
-
-# Specific
-gt3x_data_dir <- str_c(data_dir, "gt3x/", sep = "")
-gt3x_output_dir <- str_c(output_dir, "01_raw_acc_data/", sep = "")
-
-# Create output directory if it does not exist
-if (dir.exists(gt3x_output_dir) == FALSE) {
-  dir.create(gt3x_output_dir, recursive = TRUE)
-}
-
-# List files in data directory
-files <- list.files(gt3x_data_dir)
-
-# Converting gt3x into txt ------------------------------------------------
-
-for (i in 1:length(files)) {
-  # Get info from file name
-  ID_num <- str_sub(files[i], 1, 3)
-  eval_num <- str_sub(files[i], 5, 7)
-  
-  # Check if output file already exists for the current ID and convert the file
-  # only if not
-  output_file <- str_c(ID_num, eval_num, "raw.txt", sep = "_")
-  output_path <- str_c(gt3x_output_dir, output_file, sep = "")
-  if (file.exists(output_path) == FALSE) {
-    message <- str_c(
-      "Reading file ", i, " out of ", length(files), ": ", files[i], sep = ""
-    )
-    print(message)
-    # Read gt3x file (raw)
-    start_time <- Sys.time()
-    data_file <- str_c(gt3x_data_dir, files[i], sep = "")
-    gt3x <- read.gt3x(
-      data_file, imputeZeroes = TRUE, asDataFrame = TRUE
-    )
-    end_time <- Sys.time()
-    time <- round(as.numeric(difftime(end_time, start_time, units = "secs")), 1)
-    message <- str_c("Reading took ", time, " seconds", sep = "")
-    print(message)
-
-    # Select only the acc axes
-    gt3x <- gt3x[, 1:3]
-
-    print("Converting gt3x into txt")
-    # Write raw data into a txt file    
-    start_time <- Sys.time()
-    write_delim(gt3x, output_path, delim = ",")
-    end_time <- Sys.time()
-    time <- round(as.numeric(difftime(end_time, start_time, units = "secs")), 1)
-    message <- str_c("Conversion took ", time, " seconds", sep = "")
-    print(message)
-    message <- str_c("File written: ", output_file, sep = "")
-    print(message)
-  } else {
-    message <- str_c("File ", files[i], " has already been processed", sep = "")
-    print(message)
+  # Create output directory if it does not exist
+  if (dir.exists(gt3x_output_dir) == FALSE) {
+    dir.create(gt3x_output_dir, recursive = TRUE)
   }
+  
+  # List files in data directory
+  files <- list.files(gt3x_data_dir)
+  
+  # Converting gt3x into txt
+  for (i in 1:length(files)) {
+    # Get info from file name
+    ID_num <- str_sub(files[i], 1, 3)
+    eval_num <- str_sub(files[i], 5, 7)
+    
+    # Check if output file already exists for the current ID and convert the file
+    # only if not
+    output_file <- str_c(ID_num, eval_num, "raw.txt", sep = "_")
+    output_path <- str_c(gt3x_output_dir, output_file, sep = "")
+    if (file.exists(output_path) == FALSE) {
+      message <- str_c(
+        "Reading file ", i, " out of ", length(files), ": ", files[i], sep = ""
+      )
+      print(message)
+      # Read gt3x file (raw)
+      start_time <- Sys.time()
+      data_file <- str_c(gt3x_data_dir, files[i], sep = "")
+      gt3x <- read.gt3x(
+        data_file, imputeZeroes = TRUE, asDataFrame = TRUE
+      )
+      end_time <- Sys.time()
+      time <- round(as.numeric(difftime(end_time, start_time, units = "secs")), 1)
+      message <- str_c("Reading took ", time, " seconds", sep = "")
+      print(message)
+  
+      # Select only the acc axes
+      gt3x <- gt3x[, 1:3]
+  
+      print("Converting gt3x into txt")
+      # Write raw data into a txt file    
+      start_time <- Sys.time()
+      write_delim(gt3x, output_path, delim = ",")
+      end_time <- Sys.time()
+      time <- round(as.numeric(difftime(end_time, start_time, units = "secs")), 1)
+      message <- str_c("Conversion took ", time, " seconds", sep = "")
+      print(message)
+      message <- str_c("File written: ", output_file, sep = "")
+      print(message)
+    } else {
+      message <- str_c("File ", files[i], " has already been processed", sep = "")
+      print(message)
+    }
+  }
+  
+  print("Done!")
 }
 
-print("Done!")
+main()
