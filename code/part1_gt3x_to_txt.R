361afea024a724e14e55555416f8031d10df0e56
Lucas Veras <lucasdsveras@gmail.com>
2020-04-22 17:09:30 +0100

    Break main() function

--- code/part1_gt3x_to_txt.R
+++ code/part1_gt3x_to_txt.R
@@ -1,19 +1,23 @@
-main <- function(data_dir, output_dir) {
+set_paths <- function(data_dir, output_dir) {
   suppressPackageStartupMessages(require(stringr))
-  suppressPackageStartupMessages(require(read.gt3x))
-  suppressPackageStartupMessages(require(data.table))
-
+  
   # Set paths
-  gt3x_data_dir <- str_c(data_dir, "gt3x/", sep = "")
-  gt3x_output_dir <- str_c(output_dir, "part1_raw_acc_data/", sep = "")
-
+  gt3x_data_dir <<- str_c(data_dir, "gt3x/", sep = "")
+  gt3x_output_dir <<- str_c(output_dir, "part1_raw_acc_data/", sep = "")
+  
   # Create output directory if it does not exist
   if (dir.exists(gt3x_output_dir) == FALSE) {
     dir.create(gt3x_output_dir, recursive = TRUE)
   }
   
   # List files in data directory
-  files <- list.files(gt3x_data_dir)
+  files <<- list.files(gt3x_data_dir)
+}
+
+convert_gt3x <- function(files, gt3x_data_dir, gt3x_output_dir) {
+  suppressPackageStartupMessages(require(stringr))
+  suppressPackageStartupMessages(require(read.gt3x))
+  suppressPackageStartupMessages(require(data.table))
   
   # Converting gt3x into txt
   for (i in 1:length(files)) {
@@ -40,10 +44,10 @@ main <- function(data_dir, output_dir) {
       time <- round(as.numeric(difftime(end_time, start_time, units = "secs")), 1)
       message <- str_c("Reading took ", time, " seconds", sep = "")
       print(message)
-  
+      
       # Select only the acc axes
       gt3x <- gt3x[, 1:3]
-  
+      
       print("Converting gt3x into txt")
       # Write raw data into a txt file    
       start_time <- Sys.time()
@@ -63,8 +67,13 @@ main <- function(data_dir, output_dir) {
   print("Done!")
 }
 
-if(!interactive()) {
-    args <- commandArgs(trailingOnly = TRUE)
+main <- function(data_dir, output_dir) {
+  set_paths(data_dir, output_dir)
+  convert_gt3x(files, gt3x_data_dir, gt3x_output_dir)
+}
+
+if (!interactive()) {
+  args <- commandArgs(trailingOnly = TRUE)
 	data_dir <- args[1]
 	output_dir <- args[2]  
 	main(data_dir, output_dir)
